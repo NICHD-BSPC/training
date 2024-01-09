4abf73eded09135b94daf794278fd18972489096
Ryan Dale <ryan.dale@nih.gov>
2024-01-08 22:12:05 -0500

    try adding user-agent for linkcheck

--- source/conf.py
+++ source/conf.py
@@ -34,6 +34,8 @@ extensions = [
     'sphinx.ext.todo',
 ]
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+
 todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
